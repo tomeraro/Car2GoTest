5de1eae7a12b8de8c884dfe211c28d300700c2da
Gilad Daniel <gilad.daniel.dev@gmail.com>
2015-03-22 12:22:32 +0200

    FirstVersion
    First Version

---
+++ ASPTest/BL/EventsRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPTest
+{
+    public class EventsRepository : IEvents
+    {
+        List<int> Items = new List<int>();
+        Random _rand = new Random();
+        public EventsRepository()
+        {
+           
+            for (int i = 0; i < 100; i++)
+            {
+
+                Items.Add(_rand.Next(0, 100));
+            }
+        }
+
+        public int Next(int Postion)
+        {
+            return 0;
+        }
+
+        public List<int> GetAllItems()
+        {
+            return Items;
+        }
+    }
+
+    interface IEvents
+    {
+        int Next(int Postion);
+        List<int> GetAllItems();
+
+    }
+}
---
+++ ASPTest/Controllers/CalcController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPTest.Controllers
+{
+    public class CalcController : ApiController
+    {
+        IEvents _eventRep = new EventsRepository();
+        /// <summary>
+        /// Return Next Postion
+        /// </summary>
+        /// <param name="Postion"></param>
+        /// <returns></returns>
+        public int Get(int Postion)
+        {
+            return _eventRep.Next(Postion);
+        }
+
+        public List<int> Get()
+        {
+            return _eventRep.GetAllItems();
+        }
+    }
+}
